6bdb57a151fede1c388437eec0e7d05b85502755
emraftis <82290450+emraftis@users.noreply.github.com>
2021-08-21 19:10:33 -0600

    updates to VueClient styling

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="inputs">
-    <label for="industryName">Industry:</label>
-    <select name="industryName" id="industryName" v-model="selectedIndustryName" @blur="getIndustrySkills(selectedIndustryName)">
-      <option v-for="industry in industryNames" :key="industry" :value="industry">{{ industry }}</option>
-    </select>
-    <br>
     <label for="countryName">Country:</label>
-    <select name="countryName" id="countryName" v-model="selectedCountryName" @blur="getIndustryGrowth(selectedCountryName, selectedIndustryName)">
+    <select name="countryName" id="countryName" v-model="selectedCountryName" @change="getIndustryGrowth(selectedCountryName, selectedIndustryName)">
       <option v-for="country in countryNames" :key="country" :value="country">{{ country }}</option>
     </select>
+    <br>
+    <div v-if="selectedCountryName">
+      <label for="industryName">Industry:</label>
+      <select name="industryName" id="industryName" v-model="selectedIndustryName" @change="getIndustrySkills(selectedIndustryName)">
+        <option v-for="industry in industryNames" :key="industry" :value="industry">{{ industry }}</option>
+      </select>
+    </div>
     <br>
     <button @click="getIndustrySkills(selectedIndustryName)">Search</button>
   </div>
 
   <div v-if="shouldDisplay" class="top-skills">
     <h3 v-if="topSkills.length > 0">{{ industryHeadingString }}</h3>
+    <div class="list-container">
+      <p>The Industry Skills Needs metric captures which skills are most likely to be added to a member's profile in one industry compared to other industries. It's calculated using an adapted version of a text mining technique called Term Frequency - Inverse Document Frequency (TF-IDF). This method gives more weight to a skill for an industry if more members in the industry list the skill on their profiles and the skill is more unique to the industry. The skills included are those added while a member holds a particular occupation (the skill flow approach). While the skill flow approach creates a trade-off whereby long-held basic skills, such as Microsoft Office being given a lesser weight, the approach is shown to be stronger at identifying the latest emerging skills in a specific industry than including all historical skills that are added during prior occupations. On balance, since the objective of this metric is to detect the latest skills needs, a skill flow approach is adopted.</p>
+    </div>
     <div class="list-container">
       <ul v-if="topSkills.length > 0">
         <li v-for="skill in topSkills" :key="skill">{{skill}}</li>
@@ -23,9 +28,11 @@
   </div>
   
   <div v-if="shouldDisplay" class="growth-rate">
-    
     <h3>{{ growthHeadingString }}</h3>
-    <div class="chart-container">
+    <div class="list-container">
+      <p>The Growth from Industry Transitions metric looks at how LinkedIn members are moving across industries (based on net transition: in minus out). For example, if a net of 50 members joined a Biotech industry with 1,000 members in a particular country after leaving jobs in other industries, that Biotech industry would have grown by 5% due to transitions in that year. We calculate these rates across all industries on an annual basis, and report an average of the last three years. The metric above is built entirely on a sample of LinkedIn members that have a company registered on LinkedIn on their profile. Since white-collar workers in knowledge-intensive services sectors are more likely to be on LinkedIn, the growth rate above may not accurately represent sectors like manufacturing and mining that tend to have more blue-collar workers.</p>
+    </div>
+    <div class="chart-container" v-if="industryGrowth">
       <area-chart width="100%" suffix="%" xtitle="Year" ytitle="%" :data='{
         2015:industryGrowth[0].rate, 
         2016:industryGrowth[1].rate, 
@@ -34,6 +41,12 @@
         2019:industryGrowth[4].rate
         }'
       ></area-chart>
+    </div>
+    <div v-if="industryGrowth" class="list-container">
+      <caption class="caption-text"><a href="https://documents1.worldbank.org/curated/en/827991542143093021/pdf/World-Bank-Group-LinkedIn-Data-Insights-Jobs-Skills-and-Migration-Trends-Methodology-and-Validation-Results.pdf">"World Bank LinkedIn Digital Data for Development" by World Bank Group & LinkedIn Corporation, licensed under CC BY 3.0.</a></caption>
+    </div>
+    <div v-else>
+      <h4>No Data Available</h4>
     </div>
   </div>
 </template>
@@ -117,12 +130,10 @@ export default {
         "Music",
         "Health, Wellness & Fitness",
         "Animation"],
-      countryNames: ["Canada","United States","Australia","Brazil","Switzerland",
-      "Costa Rica","Cuba","Germany","Denmark","Spain","France","United Kingdom",
-      "Georgia","Greece","Hong Kong SAR, China","Ireland","Italy",
-      "Japan","Korea, Rep.","Lebanon","Mexico","Malaysia",
-      "New Zealand","Philippines","Puerto Rico","Portugal","Singapore","Thailand",
-      "Vietnam","South Africa"],
+      countryNames: ["Australia","Brazil","Canada","Costa Rica","Denmark","France",
+      "Georgia","Germany","Greece","Hong Kong SAR, China","Ireland","Italy", "Japan","Korea, Rep.","Lebanon",
+      "Mexico","Malaysia","New Zealand","United Kingdom","Philippines","Puerto Rico",
+      "Portugal","Singapore","South Africa","Spain","Switzerland","Thailand","United States","Vietnam"],
       selectedIndustryName: '',
       selectedCountryName: '',
       industryAPIString: '',
@@ -231,6 +242,7 @@ li {
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 50%
 }
 .chart-container {
   display: flex;
@@ -241,7 +253,18 @@ li {
 a {
   color: #42b983;
 }
-
+p {
+  display: flex;
+  font-size: 0.85rem;
+}
+#countryName,
+#industryName {
+  padding: 0.15rem;
+  margin: 0.3rem
+}
+.caption-text {
+  font-size: 0.7rem;
+}
 @media screen and (min-width: 850px) {
   .chart-container {
     display: flex;
@@ -254,7 +277,7 @@ a {
     align-items: center;
   }
 
-  .growth-rate {
+  .growth-rate, .top-skills {
     display: flex;
     flex-direction: column;
     justify-content: center;
